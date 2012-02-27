933f8f4a15fbc89b68c5783c048e8a40c62ea4dc
Matthias Kretschmann <desk@kremalicious.com>
2012-02-27 04:41:06 +0100

    touch icons added

---
+++ user/plugins/my-own-stuff/plugin.php
@@ -0,0 +1,51 @@
+<?php
+/*
+Plugin Name: My Own Stuff
+Plugin URI: http://mkretschmann.com
+Description: modify title, custom css file
+Version: 0.1
+Author: kremalicious
+Author URI: http://mkretschmann.com
+*/
+
+if( !defined( 'YOURLS_ABSPATH' ) ) die();
+
+// modify the <title> of pages in the admin area
+yourls_add_filter( 'html_title', 'krtmn_change_title' );
+
+function krtmn_change_title( $value, $title ) {
+	$value = $title.' | krt.mn';
+	return $value;
+}
+
+
+// modify admin logo
+//yourls_add_action( 'html_logo', 'krtmn_logo' );
+//
+//function krtmn_logo() {
+//	echo '<li><a href="http://ozh.org/">hello</a></li>';
+//}
+
+
+// custom css
+yourls_add_action( 'html_head', 'krtmn_css' );
+
+function krtmn_css() {
+	echo '<link rel="stylesheet" href="/user/css/krtmn.css" />';
+}
+
+// favicon & touch icons
+yourls_add_action( 'html_head', 'krtmn_touch_icons' );
+
+function krtmn_touch_icons() {
+	echo '<link rel="apple-touch-icon-precomposed" sizes="114x114" href="/apple-touch-icon-114x114-precomposed.png">
+		  <link rel="apple-touch-icon-precomposed" sizes="72x72" href="/apple-touch-icon-72x72-precomposed.png">
+		  <link rel="shortcut icon" href="/apple-touch-icon-precomposed.png">
+		  <link rel="shortcut icon" href="/favicon.ico">';
+}
+
+
+
+
+
+?>
